c75dda361929b8f7d760e7bc4ef4c88c7ee138ce
maja-wright <maja.filipasic@hotmail.com>
2020-11-28 11:47:33 -0800

    first commit

---
+++ date.js
@@ -0,0 +1,58 @@
+// jshint esversion:6
+
+exports.getDate = function(){
+
+const today = new Date();
+const options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+};
+
+return today.toLocaleDateString("en-US", options);
+};
+
+
+
+    // SECOND WAY
+
+    // var today = new Date();
+    // var currentDay = today.getDay();
+    // var day = "";
+
+    // switch (currentDay) {
+    //     case 0:
+    //         day = "Sunday"; 
+    //         break;
+    //     case 1:
+    //         day = "Monday";
+    //         break;
+    //     case 2:
+    //         day = "Tuesday"; 
+    //         break;
+    //     case 3:
+    //         day = "Wednesday";
+    //         break;
+    //     case 4:
+    //         day = "Thursday";
+    //         break;
+    //     case 5:
+    //         day = "Friday"; 
+    //         break;
+    //     case 6:
+    //         day = "Saturday";
+    //         break;
+    //     default:
+    //         console.log("Error: current day is equal to: " + currentDay);
+    //         break;
+    // }
+
+    exports.getDay = function(){
+
+        const today = new Date();
+        const options = {
+            weekday: "long"
+        };
+        
+        return today.toLocaleDateString("en-US", options);
+        };
